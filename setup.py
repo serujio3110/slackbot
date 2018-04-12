e589cb7bffd69ca11f8f1be82d6a710f061863f8
Adrian Muraru <amuraru@adobe.com>
2018-04-12 11:01:41 +0300

    Removed old python3 versions

--- setup.py
+++ setup.py
@@ -32,6 +32,4 @@ setup(name='slackbotng',
                    'Programming Language :: Python :: 2',
                    'Programming Language :: Python :: 2.7',
                    'Programming Language :: Python :: 3',
-                   'Programming Language :: Python :: 3.4',
-                   'Programming Language :: Python :: 3.5',
                    'Programming Language :: Python :: 3.6'])
